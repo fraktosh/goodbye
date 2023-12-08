ee977a81492674d48608ebdd2f9eef7aa0a1b39a
Soham Saha <34938615+fraktosh@users.noreply.github.com>
2023-12-08 07:22:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,49 @@
 document.addEventListener('DOMContentLoaded', function () {
-    let countDownDate = new Date("September 6 , 2024 23:59:59").getTime();
-
+    // Function to update the countdown
     function updateCountdown() {
-        const now = new Date().getTime();
-        const distance = countDownDate - now;
+        // Get the saved countdown end time from local storage or use a default value
+        let countDownDate = parseInt(localStorage.getItem('countDownDate')) || new Date("Dec 31, 2023 23:59:59").getTime();
 
-        const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        // Update the countdown every 1 second
+        const x = setInterval(function () {
+            // Get the current date and time
+            const now = new Date().getTime();
 
-        document.getElementById("countdown").innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s `;
+            // Calculate the remaining time
+            const distance = countDownDate - now;
 
-        if (distance < 0) {
-            clearInterval(x);
-            document.getElementById("countdown").innerHTML = "EXPIRED";
-        }
+            // Calculate days, hours, minutes, and seconds
+            const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+            const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+            // Display the countdown
+            document.getElementById("countdown").innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s `;
+
+            // If the countdown is over, display a message
+            if (distance < 0) {
+                clearInterval(x);
+                document.getElementById("countdown").innerHTML = "EXPIRED";
+            }
+        }, 1000);
     }
 
-    updateCountdown(); // Initial countdown update
+    // Function to add 1 minute to the countdown
+    function addMinute() {
+        let countDownDate = parseInt(localStorage.getItem('countDownDate')) || new Date("Dec 31, 2023 23:59:59").getTime();
+        countDownDate += 60000; // Add 1 minute (60,000 milliseconds)
 
-    const x = setInterval(updateCountdown, 1000);
+        // Save the updated countdown end time to local storage
+        localStorage.setItem('countDownDate', countDownDate);
 
-    // Button click event
-    document.getElementById("addMinute").addEventListener("click", function () {
-        countDownDate += 1 * 60 * 1000; // Add 1 minute (in milliseconds)
+        // Update the countdown
         updateCountdown();
-    });
+    }
+
+    // Add event listener for the button click
+    document.getElementById("addMinuteBtn").addEventListener('click', addMinute);
+
+    // Initial call to update the countdown
+    updateCountdown();
 });
